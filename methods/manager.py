4f2f83080df36d0eb9de08fed2f83bd7524018c7
ngocdaobao <dbaongoc2312@gmail.com>
2026-01-01 02:21:44 +0700

    eoe train condition

--- methods/manager.py
+++ methods/manager.py
@@ -915,7 +915,8 @@ class Manager(object):
                 self.prompted_classifier = prompted_classifier
 
                 # train
-                self.train_classifier(args, classifier, swag_classifier, self.replayed_key, "train_classifier_epoch_")
+                if args.use_eoe_tii != "yes":
+                    self.train_classifier(args, classifier, swag_classifier, self.replayed_key, "train_classifier_epoch_")
                 self.train_classifier(args, prompted_classifier, swag_prompted_classifier, self.replayed_data, "train_prompted_classifier_epoch_")
 
                 # prediction
