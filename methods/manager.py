2f10095f659a8e277a9670c71b6dfe13d9bae3b9
ngocdaobao <dbaongoc2312@gmail.com>
2026-01-01 23:56:54 +0700

    id2task

--- methods/manager.py
+++ methods/manager.py
@@ -719,7 +719,9 @@ class Manager(object):
 
         # testing
         for step, (labels, tokens, _) in enumerate(td):
+            print(self.id2taskid)
             try:
+
                 sampled += len(labels)
                 targets = labels.type(torch.LongTensor).to(args.device)
                 tokens = torch.stack([x.to(args.device) for x in tokens], dim=0)
